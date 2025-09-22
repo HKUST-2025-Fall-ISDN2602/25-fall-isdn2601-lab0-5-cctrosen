51008cb7ebcea4f66feb3dee86969e0ef829375e
cctrosen <cctrosen@connect.ust.hk>
2025-09-22 11:22:45 +0200

    Update Lab0.5_T1.ino
    Lab0.5 Task 2 - Analog Input: Read the voltage across a variable resistor

--- Lab0.5_T1.ino
+++ Lab0.5_T1.ino
@@ -1,5 +1,6 @@
 // the number of the LED pin
 const int ledPin = 5;  // 5 corresponds to GPIO5
+const int vrPin = 36; // 36 corresponds to GPIO36
 
 // setting PWM properties
 const int freq = 5000;
@@ -14,25 +15,23 @@ void setup(){
   //resolution select resolution for LEDC channel. Range is 1-14 bits (1-20 bits for ESP32).
   //The LEDC channel will be selected automatically
 
+  Serial.begin(115200);
+  // set GPIO36 as input
+  pinMode(vrPin, INPUT);
+  
   ledcAttach(ledPin, freq, resolution);
 
 }
  
 void loop(){
 
-  //bool ledcWrite(uint8_t pin, uint32_t duty);
+ // read the voltage level at GPIO36
+int vr_value = analogRead(vrPin);
+int dutyCycle = map(vr_value, 0, 4095, 0, 255);
+Serial.println(dutyCycle);
 
-  // increase the LED brightness
-  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
-    // changing the LED brightness with PWM
-    ledcWrite(ledPin, dutyCycle);
-    delay(1);
-  }
+ledcWrite(ledPin, dutyCycle);
 
-  // decrease the LED brightness
-  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
-    // changing the LED brightness with PWM
-    ledcWrite(ledPin, dutyCycle);   
-    delay(1);
-  }
+delay(100);
+
 }
